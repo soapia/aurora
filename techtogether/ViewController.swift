392a97ab74fc8accafe4c3caf63962e12bfcee5e
soapia <sofia.ongele@ealas.org>
2020-02-01 00:33:12 -0500

    Initial Commit

---
+++ techtogether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  techtogether
+//
+//  Created by Sofia Ongele on 2/1/20.
+//  Copyright © 2020 Sofia Ongele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
